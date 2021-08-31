3b6c71584253e44a4817c6fa37c32a99fd4c8f18
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-08-31 22:16:41 +0900

    add: TabBar 구성완료

---
+++ Texture-Examples/KakaoTalk-Example/KakaoTalk-Example/Source/Controllers/TabbarController.swift
@@ -0,0 +1,40 @@
+//
+//  TabbarController.swift
+//  KakaoTalk-Example
+//
+//  Created by SHIN YOON AH on 2021/08/31.
+//
+
+import UIKit
+
+class TabbarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setTabBar()
+        UITabBar.appearance().barTintColor = UIColor.secondarySystemBackground
+        UITabBar.appearance().tintColor = .darkGray
+    }
+    
+    func setTabBar(){
+        let friendTab = ViewController()
+        friendTab.tabBarItem = UITabBarItem(title: "", image: UIImage(named:"friendTabIcon"), selectedImage: UIImage(named: "friendTabIconSelected"))
+        
+        let chatTab = ViewController()
+        chatTab.tabBarItem = UITabBarItem(title: "", image: UIImage(named:"messageTabIcon"), selectedImage: UIImage(named: "messageTabIconSelected"))
+        
+        let searchTab = ViewController()
+        searchTab.tabBarItem = UITabBarItem(title: "", image: UIImage(named:"searchTabIcon"), selectedImage: UIImage(named: "searchTabIconSelected"))
+        
+        let shopTab = ViewController()
+        shopTab.tabBarItem = UITabBarItem(title: "", image: UIImage(named:"shopTabIcon"), selectedImage: UIImage(named: "shopTabIconSelected"))
+
+        let moreTab = ViewController()
+        moreTab.tabBarItem = UITabBarItem(title: "", image: UIImage(named:"detailTabIcon"), selectedImage: UIImage(named: "detailTabIconSelected"))
+        
+        let tabs =  [friendTab, chatTab, searchTab, shopTab, moreTab]
+        
+        self.setViewControllers(tabs, animated: false)
+        self.selectedViewController = friendTab
+    }
+}
---
+++ Texture-Examples/KakaoTalk-Example/KakaoTalk-Example/Source/Nodes/FriendNode.swift
@@ -0,0 +1,8 @@
+//
+//  FriendNode.swift
+//  KakaoTalk-Example
+//
+//  Created by SHIN YOON AH on 2021/08/31.
+//
+
+import Foundation
