1d6d8e1e315fb762fbdac0fc6504ef2d99d4970d
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-09-22 03:25:31 +0900

    feat: empty view 구현

--- Starbucks-Texture/Starbucks-Texture/Sources/Nodes/DetailCellNode.swift
+++ Starbucks-Texture/Starbucks-Texture/Sources/Nodes/DetailCellNode.swift
@@ -11,13 +11,19 @@ final class DetailCellNode: ASCellNode {
     // MARK: - Properties
     struct Const {
         static var titleAttribute: [NSAttributedString.Key: Any] {
-            return [.font: UIFont.systemFont(ofSize: 13.0, weight: .bold),
-                    .foregroundColor: UIColor.black]
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            return [.font: UIFont.systemFont(ofSize: 17.0, weight: .bold),
+                    .foregroundColor: UIColor.black, .paragraphStyle: paragraphStyle]
         }
         
         static var descriptionAttribute: [NSAttributedString.Key: Any] {
-            return [.font: UIFont.systemFont(ofSize: 11.0, weight: .regular),
-                    .foregroundColor: UIColor.gray]
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            return [.font: UIFont.systemFont(ofSize: 13.0, weight: .regular),
+                    .foregroundColor: UIColor.gray, .paragraphStyle: paragraphStyle]
         }
         
         static let insets: UIEdgeInsets =
@@ -30,17 +36,17 @@ final class DetailCellNode: ASCellNode {
     }
     
     private lazy var addCardButtonNode = ASButtonNode().then {
-        $0.cornerRadius = 15
+        $0.setImage(UIImage(systemName: "plus.circle"), for: .normal)
         $0.style.preferredSize = CGSize(width: UIScreen.main.bounds.size.width - 100, height: 180)
+        $0.tintColor = .lightGray
         $0.backgroundColor = .systemGray4.withAlphaComponent(0.4)
-        $0.borderColor = UIColor.lightGray.cgColor
-        $0.borderWidth = 1
+        $0.cornerRadius = 15
     }
     private lazy var titleTextNode = ASTextNode().then {
         $0.attributedText = NSAttributedString(string: "스타벅스 카드를 등록해보세요.", attributes: Const.titleAttribute)
     }
     private lazy var descriptionTextNode = ASTextNode().then {
-        $0.attributedText = NSAttributedString(string: "매장과 사이렌오더에서 쉽게 편리하게 사용할 수 있고, 별도 적립할 수 있습니다.", attributes: Const.descriptionAttribute)
+        $0.attributedText = NSAttributedString(string: "매장과 사이렌오더에서 쉽게 편리하게\n사용할 수 있고, 별도 적립할 수 있습니다.", attributes: Const.descriptionAttribute)
     }
     
     init(name: String, balance: String) {
@@ -61,27 +67,46 @@ final class DetailCellNode: ASCellNode {
     
     // MARK: Layout
     override func layoutSpecThatFits(_ constraintedSize: ASSizeRange) -> ASLayoutSpec {
-        return ASInsetLayoutSpec(
-            insets: .zero,
-            child: overlayLayoutSpec()
-        )
+        return overlayLayoutSpec()
     }
     
     private func contentLayoutSpec() -> ASLayoutSpec {
+        let topLayout = ASStackLayoutSpec(
+            direction: .vertical,
+            spacing: 30.0,
+            justifyContent: .start,
+            alignItems: .center,
+            children: [
+                addCardButtonNode,
+                titleTextNode
+            ]
+        )
+        
         return ASStackLayoutSpec(
             direction: .vertical,
             spacing: 10.0,
             justifyContent: .start,
             alignItems: .center,
             children: [
-                addCardButtonNode,
-                titleTextNode,
+                topLayout,
                 descriptionTextNode
             ]
         )
     }
     
+    private func contentInsetLayoutSpec() -> ASLayoutSpec {
+        var containerInsets: UIEdgeInsets = self.safeAreaInsets
+        containerInsets.top = 20.0
+        containerInsets.left = 20.0
+        containerInsets.right = 20.0
+        
+        return ASInsetLayoutSpec(
+            insets: containerInsets,
+            child: contentLayoutSpec()
+        )
+    }
+    
     private func overlayLayoutSpec() -> ASLayoutSpec {
-        return ASOverlayLayoutSpec(child: backview, overlay: contentLayoutSpec())
+        return ASOverlayLayoutSpec(child: backview, overlay: contentInsetLayoutSpec())
     }
 }
