4eff318be9a7cdfd5ed182d23cbd8f335ff4988c
Yoon Ah Shin <tlsdbsdk05250@gmail.com>
2021-12-01 11:22:15 +0900

    move: color literal 이동

--- Starbucks-Texture/Starbucks-Texture/Global/Literal/ColorLiteral.swift
+++ Starbucks-Texture/Starbucks-Texture/Global/Literal/ColorLiteral.swift
@@ -5,4 +5,12 @@
 //  Created by SHIN YOON AH on 2021/12/01.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    
+    @nonobjc class var seaweedGreen: UIColor {
+        return UIColor(red: 46.0 / 255.0, green: 153.0 / 255.0, blue: 95.0 / 255.0, alpha: 1.0)
+    }
+    
+}
